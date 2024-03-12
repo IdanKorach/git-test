43c28aac46bdc2dbe30ccfc578583f665812ee16
Idan Korach <idanko893@gmail.com>
2024-03-12 14:23:07 +0200

    Added f1 content into f2 without updating the delition of f1

--- f2.py
+++ f2.py
@@ -5,3 +5,12 @@ def subtract():
 
 
 subtract()
+
+
+def multiply():
+    a = float(input("Enter the value of a: "))
+    b = float(input("Enter the value of b: "))
+    print(f"Result = {a * b}")
+
+
+multiply()
